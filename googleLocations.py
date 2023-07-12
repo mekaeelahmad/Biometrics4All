1e7f894db995223a6d800c663b095ea40534924e
Mekaeel <mekaeel@berkeley.edu>
2023-07-11 17:32:31 -0700

    stashing changes because of merge conflict

---
+++ googleLocations.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from typing import List, Dict
+
+# Abbreviation to full day names mapping
+day_dict = {
+    'Su': 'Sunday',
+    'M': 'Monday',
+    'T': 'Tuesday',
+    'W': 'Wednesday',
+    'Th': 'Thursday',
+    'F': 'Friday',
+    'Sa': 'Saturday'
+}
+
+# Process individual time range string
+def process_time_range(time_range: str) -> str:
+    start, end = time_range.split('-')
+    if len(start) <= 4: # covers 'HAM', 'HPM', 'HHPM', 'HHAM'
+        start = start[:-2] + ':00' + start[-2:]
+    if len(end) <= 4:
+        end = end[:-2] + ':00' + end[-2:]
+    return start.upper() + '-' + end.upper()
+
+# Process time string
+def process_times(times: str) -> str:
+    time_ranges = times.split(',')
+    return ', '.join(process_time_range(time_range.strip()) for time_range in time_ranges)
+
+# Process full 'day:time' string
+def process_hours(hours: str) -> Dict[str, str]:
+    if not isinstance(hours, str):
+        return {day: '' for day in day_dict.values()}
+    
+    hours_list = hours.split('\n')
+    hours_dict = {day: '' for day in day_dict.values()}
+    for hour in hours_list:
+        try:
+            # Split the string only on the first ':'
+            day_str, time_str = hour.split(':', 1)
+            days = []
+            # Consecutive days
+            if '-' in day_str:
+                start_day, end_day = day_str.split('-')
+                days = list(day_dict.keys())[list(day_dict.keys()).index(start_day): list(day_dict.keys()).index(end_day)+1]
+            # Individual days
+            else:
+                days = day_str.split(',')
+            days = [day_dict[day] for day in days]
+            for day in days:
+                hours_dict[day] = process_times(time_str.strip())
+        except Exception as e:
+            print(f"Error occurred for {hour} with error {str(e)}")
+            continue
+    return hours_dict
+
+# Read the file
+df = pd.read_excel('Locations.xlsx', sheet_name='hours')
+
+# Process 'Store Hours' column
+for idx, row in df.iterrows():
+    store_hours = row['Store Hours']
+    day_hours = process_hours(store_hours)
+    for day, hours in day_hours.items():
+        df.at[idx, day] = hours
+
+# Save the DataFrame to an Excel file
+df.to_excel('Updated_Locations.xlsx', sheet_name='hours', index=False)
